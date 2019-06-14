a5627d66dc393bf8bfc0655d1ee4c9b719f0bb97
Marcelo Rodrigues Montalvão <marcelordrgs98@gmail.com>
2019-06-14 19:29:04 -0300

    Mask input

--- js/main.js
+++ js/main.js
@@ -1,26 +1,37 @@
-function consultaCep(){
+function consultaCep() {
     var value = $("#inputCep")[0].value,
         cep = value.replace(/\D+/g, ""),
         url = "https://viacep.com.br/ws/" + cep + "/json/";
     $.ajax({
         url: url,
         type: "GET",
-        success: function(response){
-            if(response.erro && response.erro === true){
+        success: function (response) {
+            if (response.erro && response.erro === true) {
                 alert("Cep inválido");
-            }else{debugger;
+            } else {
                 var arr = ["logradouro", "bairro", "localidade", "uf", "complemento", "gia", "ibge", "unidade"];
-                for(var i = 0; i < arr.length; i++){
+                for (var i = 0; i < arr.length; i++) {
                     var value = arr[i];
-                    if(response[value]){
+                    if (response[value]) {
                         $("#" + value).html(value.toLocaleUpperCase() + ": " + response[value]);
                     }
                 }
             }
         },
-        error: function(xhr, oError, message){
+        error: function (xhr, oError, message) {
             alert("Desculpe ocorreu um erro desconhecido, confira seus dados e \
             tente novamente!");
         }
     });
+}
+
+function formatar(mascara, element) {
+    var i = element.value.length;
+    var saida = mascara.substring(0, 1);
+    var texto = mascara.substring(i)
+
+    if (texto.substring(0, 1) != saida) {
+        element.value += texto.substring(0, 1);
+    }
+
 }
